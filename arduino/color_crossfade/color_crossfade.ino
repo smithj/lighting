60ca718559bcb7170877f49e18ee611b78f07d55
james smith <james@kapx.org>
2014-09-17 17:27:04 -0700

    fixed hold error. finally??? hopefully :(

--- arduino/color_crossfade/color_crossfade.ino
+++ arduino/color_crossfade/color_crossfade.ino
@@ -75,7 +75,7 @@ int grnVal = blue[1];
 int bluVal = blue[2];
 
 int wait = 1000;      // 10ms internal crossFade delay; increase for slower fades
-int hold = 6000;       // Optional hold when a color is complete, before the next crossFade
+int hold = 60;       // Optional hold when a color is complete, before the next crossFade
 int DEBUG = 1;      // DEBUG counter; if set to 1, will write values back via serial
 int loopCount = 10; // How often should DEBUG report?
 int repeat = 0;     // How many times should we loop before stopping? (0 for no stop)
@@ -117,7 +117,7 @@ void loop()
   }
   
 */  
-  int lights_on = digitalRead(13);
+  int lights_on = true; //digitalRead(13);
 
   if (DEBUG) {
     Serial.println("Light control pin: " + lights_on);
